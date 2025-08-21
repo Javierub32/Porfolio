84e21c9d49965bae39ce75531722346703e16bbe
Javierub32 <javierub38@gmail.com>
2025-08-21 17:40:12 +0200

    Added the project page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+};
